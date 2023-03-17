25c932cd156980805da34c70a2efb1d33cd4542c
Alparga <124362441+Alparga@users.noreply.github.com>
2023-03-17 13:15:51 +0100

    v1

--- dist/javadoc/package-search-index.js
+++ dist/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Alumno"},{"l":"alumnos"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"alumno"},{"l":"app.alumnos"}];updateSearchResults();
